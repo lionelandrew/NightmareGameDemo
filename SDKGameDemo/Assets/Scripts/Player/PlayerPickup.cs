2fe00f62a8608270f360d5c0b8de6afcb7466a13
Lionel Sy <lionely.sy@gmail.com>
2014-10-02 11:23:23 -0400

    added sound on player pickup

--- SDKGameDemo/Assets/Scripts/Player/PlayerPickup.cs
+++ SDKGameDemo/Assets/Scripts/Player/PlayerPickup.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PlayerPickup : MonoBehaviour {
 
     public ItemContainer itemContainer;
+    public AudioClip playerPickUp;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "DropItem")
         {
+            audio.PlayOneShot(playerPickUp);
             ItemContainerManager.MoveItem(collision.gameObject.GetComponent<ItemDataComponent>().itemData, null, itemContainer);
             Destroy(collision.gameObject);
         }
